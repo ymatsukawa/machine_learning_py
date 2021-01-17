79ac954929ee46806160b318866adf2dde12a22f
Yusuke Matsukawa <ymatsukawa27@gmail.com>
2021-01-17 18:37:47 +0900

    added comment (#2)

--- src/perceptron/perceptron.py
+++ src/perceptron/perceptron.py
@@ -6,6 +6,19 @@ class Perceptron:
     
     eta: float
         learning rate (between 0.0 and 1.0)
+        it adjusts weights with respect to loss gradient
+        ref.
+            https://heartbeat.fritz.ai/introduction-to-learning-rates-in-machine-learning-6ed685c16506
+        learning rate real life
+            If a child sees ten birds and all of them are black in color,
+            he might believe that all birds are black and would consider this as a feature when trying to identify birds.
+            Imagine next he’s shown a yellow bird, and his parents tell him that it’s a bird.
+            With a desirable learning rate, he would quickly understand that black color is not an important feature of birds and would look for another feature.
+            But with a low learning rate, he would consider the yellow bird an outlier and would continue to believe that all birds are black.
+            And if the learning rate is too high, he would instantly start to believe that all birds are yellow even though he has seen more black birds than yellow ones.
+            - Reprint source: https://heartbeat.fritz.ai/introduction-to-learning-rates-in-machine-learning-6ed685c16506
+        finding good learning rate:
+                https://towardsdatascience.com/understanding-learning-rates-and-how-it-improves-performance-in-deep-learning-d0d4059c1c10
 
     data_num: int
         training data set num
@@ -51,6 +64,12 @@ class Perceptron:
         for _ in range(self.data_num):
             errors = 0
             for xi, target in (X, y):
+                """
+                update weights [w1, w2, ... wn]
+                ref.
+                    https://lp-tech.net/articles/QUrxD
+                    https://laptrinhx.com/supervised-learning-neural-networks-714798990/#pid=9
+                """
                 update_w = self.eta * (target - self.predict(xi))
                 self._w[1:] += update_w * xi
                 self._w[0] += update_w
