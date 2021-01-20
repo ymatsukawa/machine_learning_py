2870405c67ac045bf1755f432be3d97daa32fa66
Yusuke Matsukawa <ymatsukawa27@gmail.com>
2021-01-21 00:38:27 +0900

    added essence README and wrote skeletal content (#4)
    * added essence ML and wrote skeletal content
    
    * Update src/scikit_and_perceptron/main.py
    
    * Update src/scikit_and_perceptron/main.py
    
    * Update src/scikit_and_perceptron/main.py

---
+++ src/scikit_and_perceptron/main.py
@@ -0,0 +1,23 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+"""
+choose feature value and gather traing sample
+"""
+# gather tarininng sample No.1
+## get Iris data from scikitlearn
+iris = datasets.load_iris()
+# feature value is chosen as inde-2 and index-3 of iris data
+X = iris.data[:, [2, 3]]
+# get label
+y = iris.target
+
+# gather tarininng sample No.2
+## split data for training and will be tested
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=1, stratify=y
+)
+
+# scale or standardize sample
+
+# machine learn
